1b59dec3e003647bc4ee324146f90cb198fa46c1
DuckXYZMods <itsduckxyz@outlook.com>
2024-10-03 20:01:47 +0100

    learn single spell command

--- src/main/java/com/ombremoon/spellbound/common/commands/LearnSkillsCommand.java
+++ src/main/java/com/ombremoon/spellbound/common/commands/LearnSkillsCommand.java
@@ -1,9 +1,11 @@
 package com.ombremoon.spellbound.common.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.ombremoon.spellbound.common.data.SkillHandler;
 import com.ombremoon.spellbound.common.data.SpellHandler;
 import com.ombremoon.spellbound.common.init.DataInit;
+import com.ombremoon.spellbound.common.init.SkillInit;
 import com.ombremoon.spellbound.common.init.SpellInit;
 import com.ombremoon.spellbound.common.magic.SpellType;
 import com.ombremoon.spellbound.common.magic.skills.Skill;
@@ -16,15 +18,36 @@ import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 
 public class LearnSkillsCommand {
+
     public LearnSkillsCommand(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext context) {
         dispatcher.register(Commands.literal("learnskills")
                 .then(Commands.argument("spell", ResourceArgument.resource(context, SpellInit.SPELL_TYPE_REGISTRY_KEY))
                         .executes(cmdContext -> learnSpells(cmdContext.getSource(),
                                 ResourceArgument.getResource(cmdContext, "spell", SpellInit.SPELL_TYPE_REGISTRY_KEY))))
-                .then(Commands.literal("reset")
-                        .then(Commands.argument("spell", ResourceArgument.resource(context, SpellInit.SPELL_TYPE_REGISTRY_KEY))
+                .then(Commands.argument("spell", ResourceArgument.resource(context, SpellInit.SPELL_TYPE_REGISTRY_KEY))
+                        .then(Commands.literal("reset")
                                 .executes(cmdContext -> resetSpells(cmdContext.getSource(),
-                                        ResourceArgument.getResource(cmdContext, "spell", SpellInit.SPELL_TYPE_REGISTRY_KEY))))));
+                                        ResourceArgument.getResource(cmdContext, "spell", SpellInit.SPELL_TYPE_REGISTRY_KEY)))))
+                .then(Commands.argument("spell", ResourceArgument.resource(context, SpellInit.SPELL_TYPE_REGISTRY_KEY))
+                        .then(Commands.argument("skill", ResourceArgument.resource(context, SkillInit.SKILL_REGISTRY_KEY))
+                                .executes(cmdContext -> learnSingleSkill(cmdContext.getSource(),
+                                        ResourceArgument.getResource(cmdContext, "spell", SpellInit.SPELL_TYPE_REGISTRY_KEY),
+                                        ResourceArgument.getResource(cmdContext, "skill", SkillInit.SKILL_REGISTRY_KEY))))));
+    }
+
+    private int learnSingleSkill(CommandSourceStack context, Holder.Reference<SpellType<?>> spell, Holder.Reference<Skill> skill) {
+        if (!context.isPlayer()) return 0;
+
+        if (!SpellUtil.getSpellHandler(context.getPlayer()).getSpellList().contains(spell)) {
+            context.getPlayer().sendSystemMessage(Component.translatable("command.spellbound.nospellknown"));
+            return 0;
+        }
+
+        SpellUtil.getSkillHandler(context.getPlayer()).unlockSkill(skill.value());
+        SpellUtil.getSkillHandler(context.getPlayer()).sync(context.getPlayer());
+        context.getPlayer().sendSystemMessage(Component.translatable("command.spellbound.singlespelllearnt"));
+
+        return 1;
     }
 
     private int learnSpells(CommandSourceStack context, Holder.Reference<SpellType<?>> spell) {
